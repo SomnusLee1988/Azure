92f04d4c351024952a640f8a934f99e1e4076fd9
SomnusLee1988 <somnuslee1988@126.com>
2016-07-05 01:06:54 +0800

    init commit
    init commit

---
+++ Azure/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Azure
+//
+//  Created by Somnus on 16/7/5.
+//  Copyright © 2016年 Somnus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,6 @@
+platform :ios, '8.0'
+use_frameworks!
+target 'Azure' do
+pod 'AFNetworking', '~> 3.0'
+pod 'CocoaLumberjack/Swift', '~> 2.3.0'
+end
